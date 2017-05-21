43f96208bce6f812b8423029de9469aeb2b388bb
Einstein <TechCorp07@users.noreply.github.com>
2017-05-21 08:25:57 +0200

    Add files via upload

---
+++ shopifyfusion/yourappsfolder/catalogue/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from oscar.apps.catalogue.abstract_models import AbstractProduct
+
+class Product(AbstractProduct):
+    active = models.BooleanField(default=False)
+
+from oscar.apps.catalogue.models import *
